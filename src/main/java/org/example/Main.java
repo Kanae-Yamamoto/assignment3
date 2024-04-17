77e91b0197d32e20353a406ef3fa08ec48fc6a5e
Kanae-Yamamoto <chankana0504@yahoo.co.jp>
2024-04-17 16:37:48 +0900

    Main.javaに患者情報を1名追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,7 @@ public class Main {
         userMap.put("田中", "67歳。既往歴は大腸ポリープ。内視鏡手術により治療済みです。");
         userMap.put("鈴木", "71歳。既往歴はCOPD。酸素ボンベを使用中です。");
         userMap.put("横田", "43歳。既往歴は脂質異常症。内服薬にて治療中です。");
+        userMap.put("西川", "35歳。既往歴は逆流性食道炎。内服薬にて治療中です。");
 
         for (String key : userMap.keySet()) {
             System.out.println(key + "さんは" + userMap.get(key));
